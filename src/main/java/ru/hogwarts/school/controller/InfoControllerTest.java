0f3a23ce4094812afadbcb8686d8bf087c223133
[M210®] <m210-2007@mail.ru>
2022-04-29 22:20:29 +0300

    Homework 4.4 hotfix

--- src/main/java/ru/hogwarts/school/controller/InfoControllerTest.java
+++ src/main/java/ru/hogwarts/school/controller/InfoControllerTest.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Profile("test")
 public class InfoControllerTest implements InfoController {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("/getPort")
     public ResponseEntity<Integer> getPort() {
-        return ResponseEntity.ok(1234);
+        return ResponseEntity.ok(port);
+    }
+
+    @GetMapping()
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Just for test!");
     }
 }
